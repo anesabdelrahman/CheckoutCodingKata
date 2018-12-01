3fcf23e21b9c47dfe4318ff0ae033799b166695c
Anes Abdelrahman <anes.abdelrahman@gmail.com>
2018-12-01 11:42:07 +0000

    Added more specific tests incase scanned items are of multiples of special prices' quantities i.e. Product A > 3 or Product B > 2

--- SupermarketCheckout/CheckoutTests/CheckoutTest.cs
+++ SupermarketCheckout/CheckoutTests/CheckoutTest.cs
@@ -24,6 +24,7 @@ namespace CheckoutTests
         [TestCase("B", 1, 30)]
         [TestCase("B", 2, 45)]
         [TestCase("B", 3, 75)]
+        [TestCase("B", 4, 90)]
         [TestCase("C", 1, 20)]
         [TestCase("D", 1, 15)]
         public void WhenScanSingleProducts_ItShouldTotalPriceAndAdjustForSpecialPriceDiscount(string sku, int productQuantity, int expectedPrice)
@@ -34,6 +35,38 @@ namespace CheckoutTests
                 sut.Scan(sku);
             }
             
+            var result = sut.GetTotalPrice();
+
+            //Assert
+            Assert.AreEqual(result, expectedPrice);
+
+        }
+
+        [Test]
+        [Category("Product A & B")]
+        [TestCase("A", 1, "B", 1, 80)]
+        [TestCase("A", 2, "B", 1, 130)]
+        [TestCase("A", 3, "B", 1, 160)]
+        [TestCase("A", 4, "B", 1, 210)]
+        [TestCase("A", 5, "B", 1, 260)]
+        [TestCase("A", 6, "B", 1, 290)]
+        [TestCase("A", 1, "B", 2, 95)]
+        [TestCase("A", 1, "B", 3, 125)]
+        [TestCase("A", 1, "B", 4, 140)]
+        [TestCase("A", 1, "B", 5, 170)]
+        public void WhenScan_A_and_B_ItShouldTotalPricesAndAdjustForSpecialPricesDiscounts(string productA, int productAQuantity, string productB, int productBQuantity, int expectedPrice)
+        {
+            //Act
+            for (int i = 0; i < productAQuantity; i++)
+            {
+                sut.Scan(productA);
+            }
+
+            for (int i = 0; i < productBQuantity; i++)
+            {
+                sut.Scan(productB);
+            }
+
             var result = sut.GetTotalPrice();
 
             //Assert
